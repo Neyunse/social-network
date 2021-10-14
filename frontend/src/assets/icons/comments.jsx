1e43e6ced50442b14e9185778f1e30fa7a0b6283
KagariSoft-dev <dev@kagarisoft.com>
2021-10-14 17:57:42 -0300

    fix bug

--- frontend/src/assets/icons/comments.jsx
+++ frontend/src/assets/icons/comments.jsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 
 function CommentsIcon(props) {
-    return (
-        <>
-            <svg
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                height="1em"
-                width="1em"
-                {...props}
-            ><path d="M20 1.999H4c-1.103 0-2 .897-2 2v18l4-4h14c1.103 0 2-.897 2-2v-12c0-1.103-.897-2-2-2zm-6 11H7v-2h7v2zm3-4H7v-2h10v2z"></path></svg>
-        </>
-    );
+  return (
+    <>
+      <svg
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        height="1em"
+        width="1em"
+        {...props}
+      ><path d="M20 1.999H4c-1.103 0-2 .897-2 2v18l4-4h14c1.103 0 2-.897 2-2v-12c0-1.103-.897-2-2-2zm-6 11H7v-2h7v2zm3-4H7v-2h10v2z"></path></svg>
+    </>
+  );
 }
 
 export default CommentsIcon;
