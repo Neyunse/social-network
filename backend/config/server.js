b127d7b5a9bff00f554083bab5c7f8311ca7bad4
KagariSoft-dev <dev@kagarisoft.com>
2021-10-20 18:42:57 -0300

    update api

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('', 'http://localhost:1337'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '624d6684f21a0ca8660e60ff7c25a0fa'),
